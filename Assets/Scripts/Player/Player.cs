4c01168b62d677c283ebfe6066424180e763a0e0
Crystal Jin <kstjxy@gmail.com>
2022-07-12 16:14:00 +0800

    Initialized player class

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player 
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum PlayerRole
     {
-        
+        button,         //庄家
+        bigBlind,       //大盲
+        smallBlind,     //小盲
+        normal,         //普通玩家
+        outOfGame       //不在此轮游戏中
     }
 
-    // Update is called once per frame
-    void Update()
+    public string playerName;
+    public int seatNum = -1; //如果不在此轮游戏中，则为-1
+    public int coin = 0; 
+    public bool isFold = false;
+    public bool isAllIn = false;
+    public PlayerRole role;
+    public List<Card> playerCardList;
+    public int state = -1; //no action
+
+    public Player(string playerName)
     {
-        
+        this.playerName = playerName;
+        this.playerCardList = new List<Card>();
     }
+
+    
 }
