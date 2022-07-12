70014fc3adb91605e07b94bbbc43b2ceb9d57f0d
endurian <59466746+enendurian@users.noreply.github.com>
2022-07-12 16:13:09 +0800

    Card Class
    Start scripting card class and card manager class

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //卡牌花色
+    public CardSuit cardSuit;
+    //卡面大小 from 2 to 13(K) 14(A)
+    private int value;
+    #region Get Set 限制
+    public int Value
     {
-        
+        get
+        {
+            return value;
+        }
+        set
+        {
+            if (value < 2)
+                this.value = 2;
+            else if (value > 14)
+                this.value = 14;
+            else
+                this.value = value;
+        }
+    }
+    #endregion
+
+    public Card(CardSuit cs, int vl)
+    {
+        cardSuit = cs;
+        if (vl < 2)
+            this.value = 2;
+        else if (vl> 14)
+            this.value = 14;
+        else
+            this.value = vl;
     }
 
-    // Update is called once per frame
-    void Update()
+    //从Resources中读取本卡牌对应的素材图片
+    public Sprite GetSpriteSurface()
     {
-        
+        string valueString;
+        switch (value) 
+        {
+            case 14:
+                valueString = "A";
+                break;
+            case 11:
+                valueString = "J";
+                break;
+            case 12:
+                valueString = "Q";
+                break;
+            case 13:
+                valueString = "K";
+                break;
+            default:
+                valueString = value.ToString();
+                break;
+        }
+        return Resources.Load<Sprite>("Cards/" + cardSuit.ToString() + "s/" + cardSuit.ToString() + "_" + valueString);
     }
+    
+}
+public enum CardSuit
+{ 
+    club,
+    spade,
+    diamon,
+    heart
 }
--- Assets/Scripts/Card/CardManager.cs
+++ Assets/Scripts/Card/CardManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //单例模式
+    public static CardManager instance;
+    //牌组
+    private List<Card> cards;
+
+    private void Awake()
     {
-        
+        if (instance != null)
+            Destroy(this);
+        else
+            instance = this;
+
+        InitialCardsList();
     }
 
-    // Update is called once per frame
-    void Update()
+    //加载牌堆 在
+    private void InitialCardsList()
     {
-        
+        cards.Clear();
+        for (int i = 2; i < 15; i++)
+        {
+            cards.Add(new Card(CardSuit.club, i));
+            cards.Add(new Card(CardSuit.diamon, i));
+            cards.Add(new Card(CardSuit.heart, i));
+            cards.Add(new Card(CardSuit.spade, i));
+        }
+    }
+
+    //发牌
+    public void DealCards()
+    { 
+        //根据GameManager的状态机来判断发的是什么牌
     }
 }
