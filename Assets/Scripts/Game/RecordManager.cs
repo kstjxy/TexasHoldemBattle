990f99038e94c9dd2a363724504339f0fd5bc7ad
endurian <59466746+enendurian@users.noreply.github.com>
2022-07-18 15:19:51 +0800

    Two Recording List Added

---
+++ Assets/Scripts/Game/RecordManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordManager : MonoBehaviour
+{
+    //RecordManager用来协助记录玩家操作信息。后续可以合并到GameManager中去，只需要把这里提供的接口和其他脚本中引用这里的部分稍作修改即可。
+
+    //定义事件
+    public delegate void ActionCall(int playerNum, int actionNum);//请注意：这里的 playerNum 输入的是那个玩家的 seatNum 。如果需要更改，记得要连 Player 类中的 AddActionRecord() 一并修改
+    public event ActionCall ActionRecords;
+    public static RecordManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(this.gameObject);
+        else
+            instance = this;
+    }
+
+    public void CallActionRecord(int playerNum,int actionNum)
+    {
+        if (ActionRecords != null)
+            ActionRecords(playerNum, actionNum);
+        else
+            Debug.Log("NULL delegate");
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -23,14 +23,22 @@ public class Player
     public bool isFold = false;
     public bool isAllIn = false;
     public PlayerRole role = PlayerRole.outOfGame;
-    public List<Card> playerCardList = new List<Card>();
+    public List<Card> playerCardList = new List<Card>();//两张手牌
     public int state = -1; //no action
-    public List<Card> finalCards = new List<Card>();
+    public List<Card> finalCards = new List<Card>();//最终组成的五张牌组
+
+    public List<int[,]> last_period = new List<int[,]>();//上一轮中玩家自己位置之后的玩家的行动记录（记录为【玩家序号(seatNum)，操作序号】，注册进事件来维护这两个列表）       在自己的回合结束后清空该列表以接收接下来的信息
+    public List<int[,]> this_period = new List<int[,]>();//这一轮中从庄家到玩家自己位置的行动记录（同上）      在本轮结束后（这一轮即将开始时）清空该列表以接收接下来的信息
+
     public PlayerObject playerObject;
+
 
     public Player(string playerName)
     {
         this.playerName = playerName;
+
+        //在进行初始化的时候就要注册进事件中去！接下来只要调用RecordManager中的ActionRecord就可以让所有玩家接收到信息。
+        RecordManager.instance.ActionRecords += AddActionRecord;
     }
 
     /// <summary>
@@ -66,5 +74,19 @@ public class Player
         state = -1;
         playerCardList = new List<Card>();
         finalCards = new List<Card>();
+    }
+
+    public void AddActionRecord(int playerNum, int actionNum)
+    {
+        if (playerNum == seatNum)//如果是自己的行动，不需要记录
+            return;
+        if (playerNum < seatNum)//是自己之前的玩家进行行动 this_period
+        {
+            Debug.Log("PlayerNum" + playerNum + "  ActionNum" + actionNum+"    this");
+        }
+        else//是自己之后的玩家进行行动 last_period
+        {
+            Debug.Log("PlayerNum" + playerNum + "  ActionNum" + actionNum + "    last");
+        }
     }
 }
