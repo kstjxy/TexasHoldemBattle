7535f581c19c85a4ba624e1ddbcf77df29d55dab
Crystal Jin <kstjxy@gmail.com>
2022-07-18 15:34:06 +0800

    Testing recordManager

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -179,6 +179,7 @@ public class GameManager: MonoBehaviour
         } else
         {
             UpdateCurPlayer();
+            RecordManager.instance.CallActionRecord(1, 2);
         }
     }
     public void Turn()
